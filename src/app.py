9f27bac6ea195c5fc89cd09acc9beaf06904edc9
Hong Phat <hongphat@example.com>
2025-08-25 03:57:38 +0700

    Remove pyc and __pycache__ files from git

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify
-from api.swagger import spec
-from api.controllers.todo_controller import bp as todo_bp
-from api.middleware import middleware
-from api.responses import success_response
-from infrastructure.databases import init_db
-from config import Config
+from src.api.swagger import spec
+from src.api.controllers.todo_controller import bp as todo_bp
+from src.api.middleware import middleware
+from src.api.responses import success_response
+from src.infrastructure.databases import init_db
+from src.config import Config
 from flasgger import Swagger
-from config import SwaggerConfig
+from src.config import SwaggerConfig
 from flask_swagger_ui import get_swaggerui_blueprint
 
 
--- src/infrastructure/databases/__init__.py
+++ src/infrastructure/databases/__init__.py
@@ -1,7 +1,7 @@
 from src.infrastructure.databases.mssql import init_mssql
-from infrastructure.models import todo_model
+from src.infrastructure.models import todo_model
 
 def init_db(app):
     init_mssql(app)
     
-from infrastructure.databases.mssql import Base
+from src.infrastructure.databases.mssql import Base
